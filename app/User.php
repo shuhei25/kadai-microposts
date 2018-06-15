dc4d18412a9cf8b0a8427487c944efd1b3bc1a43
Shuhei.Horiba <syu1010.soccer@gmail.com>
2018-06-15 04:14:10 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -44,44 +44,51 @@ class User extends Authenticatable
     }
     
     public function follow($userId)
-{
-    // confirm if already following
-    $exist = $this->is_following($userId);
-    // confirming that it is not you
-    $its_me = $this->id == $userId;
-
-    if ($exist || $its_me) {
-        // do nothing if already following
-        return false;
-    } else {
-        // follow if not following
-        $this->followings()->attach($userId);
-        return true;
+    {
+        // confirm if already following
+        $exist = $this->is_following($userId);
+        // confirming that it is not you
+        $its_me = $this->id == $userId;
+    
+        if ($exist || $its_me) {
+            // do nothing if already following
+            return false;
+        } else {
+            // follow if not following
+            $this->followings()->attach($userId);
+            return true;
+        }
     }
-}
 
-public function unfollow($userId)
-{
-    // confirming if already following
-    $exist = $this->is_following($userId);
-    // confirming that it is not you
-    $its_me = $this->id == $userId;
-
-
-    if ($exist && !$its_me) {
-        // stop following if following
-        $this->followings()->detach($userId);
-        return true;
-    } else {
-        // do nothing if not following
-        return false;
+    public function unfollow($userId)
+    {
+        // confirming if already following
+        $exist = $this->is_following($userId);
+        // confirming that it is not you
+        $its_me = $this->id == $userId;
+    
+    
+        if ($exist && !$its_me) {
+            // stop following if following
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            // do nothing if not following
+            return false;
+        }
     }
-}
-
-
-public function is_following($userId) {
-    return $this->followings()->where('follow_id', $userId)->exists();
-}
+    
+    
+    public function is_following($userId) {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+    
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
 
 
